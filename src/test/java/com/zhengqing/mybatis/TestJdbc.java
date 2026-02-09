3393e8f0252e96ef919cc5581d234e218aecfb7f
Cortalo <Cortalo@outlook.com>
2026-02-09 19:34:45 +0100

    原生jdbc

---
+++ src/test/java/com/zhengqing/mybatis/TestJdbc.java
@@ -0,0 +1,42 @@
+package com.zhengqing.mybatis;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * <p> 原生JDBC </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2024/4/20 18:55
+ */
+public class TestJdbc {
+
+    @Test
+    public void test() throws Exception {
+        // 1、加载JDBC驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 2、建立db连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/online_shopping?useUnicode=true&characterEncoding=UTF8&useSSL=false", "root", "root");
+
+        // 3、构建sql & 执行 -- 查询
+        PreparedStatement ps = connection.prepareStatement("select * from online_shopping_user");
+        ps.execute();
+
+        // 4、拿到结果集
+        ResultSet rs = ps.getResultSet();
+        while (rs.next()) {
+            System.out.println(rs.getString("name") + " -- " + rs.getString("email"));
+        }
+
+        // 5、释放资源
+        rs.close();
+        ps.close();
+        connection.close();
+    }
+
+}
