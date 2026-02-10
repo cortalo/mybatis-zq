98ed47df5341df60a031cce16b663a3cfdeba80e
Cortalo <Cortalo@outlook.com>
2026-02-10 10:02:04 +0100

    feat UserMapper方法参数值获取

--- src/main/java/com/zhengqing/demo/mapper/UserMapper.java
+++ src/main/java/com/zhengqing/demo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.zhengqing.demo.mapper;
 
 import com.zhengqing.demo.entity.User;
+import com.zhengqing.mybatis.annotations.Param;
 import com.zhengqing.mybatis.annotations.Select;
 
 import java.util.List;
@@ -15,6 +16,6 @@ import java.util.List;
 public interface UserMapper {
 
     @Select("select * from online_shopping_user where user_id = #{user_id} and name = #{name}")
-    List<User> selectList(Integer user_id, String name);
+    List<User> selectList(@Param("user_id") Integer user_id, @Param("name") String name);
 
 }
--- src/main/java/com/zhengqing/mybatis/binding/MapperProxy.java
+++ src/main/java/com/zhengqing/mybatis/binding/MapperProxy.java
@@ -1,5 +1,7 @@
 package com.zhengqing.mybatis.binding;
 
+import com.google.common.collect.Maps;
+import com.zhengqing.mybatis.annotations.Param;
 import com.zhengqing.mybatis.annotations.Select;
 import com.zhengqing.mybatis.parsing.GenericTokenParser;
 import com.zhengqing.mybatis.parsing.ParameterMappingTokenHandler;
@@ -7,10 +9,13 @@ import lombok.SneakyThrows;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p> mapper代理 </p>
@@ -32,11 +37,30 @@ public class MapperProxy implements InvocationHandler {
         ParameterMappingTokenHandler parameterMappingTokenHandler = new ParameterMappingTokenHandler();
         GenericTokenParser genericTokenParser = new GenericTokenParser("#{", "}", parameterMappingTokenHandler);
         String sql = genericTokenParser.parse(originalSql);
+        List<String> parameterMappings = parameterMappingTokenHandler.getParameterMappings();
 
         // 构建sql & 执行
         PreparedStatement ps = connection.prepareStatement(sql);
-        ps.setInt(1, (Integer) args[0]);
-        ps.setString(2, String.valueOf(args[1]));
+
+        // 获取mapper调用方法的参数名 -> 参数值
+        Map<String, Object> paramValueMap = Maps.newHashMap();
+        Parameter[] parameterList = method.getParameters();
+        for (int i = 0; i < parameterList.length; i++) {
+            Parameter parameter = parameterList[i];
+            Param param = parameter.getAnnotation(Param.class);
+            String paramName = param.value();
+            paramValueMap.put(paramName, args[i]);
+        }
+
+        // 设置值
+        for (int i = 0; i < parameterMappings.size(); i++) {
+            String jdbcColumnName = parameterMappings.get(i);
+            Object val = paramValueMap.get(jdbcColumnName);
+            ps.setObject(i + 1, val);
+
+//            ps.setInt(1, (Integer) val);
+        }
+
         ps.execute();
 
         // 拿到结果集
