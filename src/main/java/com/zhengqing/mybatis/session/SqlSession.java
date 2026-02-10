14df96435f170ccac96e76a167499a2b0070a5f6
Cortalo <Cortalo@outlook.com>
2026-02-10 21:04:43 +0100

    feat SqlSession增删改查

---
+++ src/main/java/com/zhengqing/mybatis/session/SqlSession.java
@@ -0,0 +1,24 @@
+package com.zhengqing.mybatis.session;
+
+import java.util.List;
+
+/**
+ * <p> 操作增删改查 </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2024/4/23 01:21
+ */
+public interface SqlSession {
+
+    int insert(String statementId, Object parameter);
+
+    int delete(String statementId, Object parameter);
+
+    int update(String statementId, Object parameter);
+
+    <T> T selectOne(String statementId, Object parameter);
+
+    <T> List<T> selectList(String statementId, Object parameter);
+
+}
